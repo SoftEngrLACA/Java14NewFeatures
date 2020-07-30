b5fd046c89d195f0cbe403d72d0efbbc9d5449c4
SoftEngrLACA <wick_sw_engineer@yahoo.com>
2020-07-30 09:34:50 -0700

    Image Handler Convert image file

---
+++ src/main/java/com/wickcentral/parallelstreams/BufferedImageHandler.java
@@ -0,0 +1,29 @@
+package com.wickcentral.parallelstreams;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class BufferedImageHandler {
+	
+	public static ByteArrayOutputStream getOutputStreamForImage(File imageFile, String imageFormatName) throws IOException {
+		
+		  //read image file from local storage
+		BufferedImage bufferedImage = ImageIO.read(imageFile);
+		
+		// write it to byte array in-memory for image format
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		if (!ImageIO.write(bufferedImage, imageFormatName, outStream)) {
+			throw new IOException("Wirting image '" + imageFile.getAbsolutePath() + "' caused exception");
+		}
+		byte[] jpgBytArray = outStream.toByteArray();
+		
+		bufferedImage.flush();
+		
+		return null;
+	}
+
+}
